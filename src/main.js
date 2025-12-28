67419141a3c41566a4cdf7130b3dcd16a88618a4
Artem <artemtemchenko7@gmail.com>
2025-12-28 15:35:45 +0200

    feat: header mobile done

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+const burger = document.querySelector('#burger')
+const mobileContainer = document.querySelector('#mobile-container')
+
+burger.addEventListener('click', ()=> {
+    burger.classList.toggle('is-active')
+    mobileContainer.classList.toggle('hidden')
+})
