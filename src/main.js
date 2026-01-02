659cb0570869e57a5cabe5f9bd9feeba36931cda
Artem <artemtemchenko7@gmail.com>
2026-01-03 01:07:05 +0200

    build: install swiper

--- src/main.js
+++ src/main.js
@@ -1,9 +1,36 @@
+import './main.scss'
 import './style.css'
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 const burger = document.querySelector('#burger')
 const mobileContainer = document.querySelector('#mobile-container')
+const body = document.querySelector('#body')
 
 burger.addEventListener('click', ()=> {
     burger.classList.toggle('is-active')
     mobileContainer.classList.toggle('hidden')
+    body.classList.toggle('overlay')
 })
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
